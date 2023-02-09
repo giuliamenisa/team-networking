ad7c44b8d2a6c0b4383f59fc8dfec895504e5f24
Faragau Giulia <giuliamenisa49@gmail.com>
2023-02-09 21:50:48 +0200

    create team

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-fetch("teams.json")
+fetch("http://localhost:3000/teams-json", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
   .then((r) => r.json())
   .then((teams) => {
     displayTeams(teams);
@@ -11,10 +16,41 @@ function displayTeams(teams) {
            <td>${team.promotion}</td>
            <td>${team.members}</td>
            <td>${team.name}</td>
-           <td>${team.github}</td>
+           <td>${team.url}</td>
            <td></td>
        </tr>`
   );
   document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
-  console.info("display", teamsHTML, teams);
+  //console.info("display", teamsHTML, teams);
 }
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      promotion: document.getElementById("promotion").value,
+      members: document.getElementById("members").value,
+      name: document.getElementById("name").value,
+      url: document.getElementById("url").value,
+    }),
+  })
+    .then((r) => r.json())
+    .then((status) => {
+      console.warn("status", status.success, status.id);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  form.addEventListener("submit", onSubmit);
+}
+
+initEvents();
