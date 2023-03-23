3927ec2a1001122bf490abc13dee3f205f7f1780
Faragau Giulia <giuliamenisa49@gmail.com>
2023-03-23 20:37:28 +0200

    use destraturing

--- src/index.js
+++ src/index.js
@@ -48,11 +48,11 @@ function readTeam() {
   };
 }
 
-function write(team) {
-  document.getElementById("promotion").value = team.promotion;
-  document.getElementById("members").value = team.members;
-  document.getElementById("name").value = team.name;
-  document.getElementById("url").value = team.url;
+function writeTeam({ promotion, members, name, url }) {
+  document.getElementById("promotion").value = promotion;
+  document.getElementById("members").value = members;
+  document.getElementById("name").value = name;
+  document.getElementById("url").value = url;
 }
 
 function getTeamsHTML(teams) {
